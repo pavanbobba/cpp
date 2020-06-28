5fbde283ee4185c9199e6fbd021edf48d4ef9fa8
Pavan Bobba <pavanbobba206@gmail.com>
2020-06-28 15:07:57 +0530

    final lib_parser

--- lib_parser.cpp
+++ lib_parser.cpp
@@ -1,7 +1,14 @@
+/***************************************************
+ * Name    : lib_parser.cpp
+ * 
+ * DESC    : parse given path for so libs and print
+ *           architecture information for each lib
+ * 
+ * Author  : pavanbobba206@gmail.com
+*****************************************************/
 #include <iostream>
 #include <string>
 #include <sys/types.h>
-#include <bits/stdc++.h> 
 #include <dirent.h>
 #include <fstream>
 #include <elf.h>
@@ -13,6 +20,10 @@ using namespace std;
 
 #define SPACE 40
 
+/*function to get the architecture information based on machine type
+type - machine type
+val - string parameter to retrieve architecture specific data
+*/
 string * get_arch_type( uint16_t type, string * val)
 {
    switch(type)
@@ -187,41 +198,59 @@ int main(int argc, char *argv[])
    unsigned char buf[20];
    map<string, string> print_data;
    DIR * dir = NULL;
+   /*variable to return number of valid libraries count*/
    unsigned int lib_count = 0;
    struct dirent * file_data = NULL;
+   /*ELF Magic first 4 bytes: 0x7f, 'E', 'L', 'F'*/
    unsigned char elf_magic[4] = {0x7f, 0x45, 0x4c, 0x46};
 
+   /*Reading the user input path from console*/
    cout<<"enter the path:"<<endl;
    getline(cin, lib_path);
 
+   /*opening the path provided by user*/
    dir = opendir(lib_path.c_str());
    if(dir != NULL)
-   {
+   { 
+      /*changing the present working directory to the path provided by user*/
       chdir(lib_path.c_str());
+
+      /*reading the entries in the directory*/
       file_data = readdir(dir);
       while (file_data != NULL)
       {
+         /*opening the file*/
          file.open(file_data->d_name);
          if(file)
          {
             memset(buf, 0, sizeof(buf));
+
+            /*Reading first 20 bytes of ELF header into buffer*/
             file.read((char *)buf, sizeof(buf));
+
+            /*validating the file having valid ELF header*/
             if(!memcmp(elf_magic, buf, sizeof(elf_magic)))
             {
                 f_type = 0;
                 m_type = 0;
+                
+                /*checking architecture byte order for which the file generated for*/
                 if(buf[5] == ELFDATA2LSB)
                 {
+                  /*concatinating bytes to find file type and machine name in little endian architecture*/
                   f_type = (((buf[17] | f_type)<<8) | buf[16]);
                   m_type = (((buf[19] | m_type)<<8) | buf[18]);
                 }
                 else if(buf[5] == ELFDATA2MSB)
                 {
+                  /*concatinating bytes to find file type and machine name in big endian architecture*/
                   f_type = (((buf[16] | f_type)<<8) | buf[17]);
                   m_type = (((buf[18] | m_type)<<8) | buf[19]);
                 }
+                /*validating whether the existing file is a shared object or not*/
                 if(f_type == ET_DYN)
                 {
+                   /*retrieving the architecture specific info in string format*/
                    get_arch_type(m_type, &str_arch);
                    print_data.insert({file_data->d_name, str_arch});
                    lib_count++;
